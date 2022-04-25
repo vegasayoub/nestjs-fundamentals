6aa9e112115a6b403c069e48d5c2449cca1a9c31
ayoubphiucs <ayoub@phicus.es>
2022-04-25 06:24:42 +0200

    CRUD for coffees service

--- ilovecoffe/src/coffees/coffees.service.ts
+++ ilovecoffe/src/coffees/coffees.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-  create(createCoffeeDto: CreateCoffeeDto) {
-    return 'This action adds a new coffee';
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Cappuccino',
+      brand: 'Starbucks',
+      flavors: ['vanilla', 'caramel'],
+    }
+  ];
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
   }
 
   findAll() {
-    return `This action returns all coffees`;
+    return this.coffees;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} coffee`;
+    return this.coffees.find(coffee => coffee.id === id);
   }
 
   update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
-    return `This action updates a #${id} coffee`;
+    const coffee = this.findOne(id);
+    if (coffee) {
+      // Find it and update it
+    }
   }
 
   remove(id: number) {
-    return `This action removes a #${id} coffee`;
+    const coffeeIndex = this.coffees.findIndex(coffee => coffee.id === id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
   }
 }
